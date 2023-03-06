6fc1c4e7e7f86b32aab82b7cda00c62c48d61532
Bobby Galli <bobbyg603@gmail.com>
2023-03-05 22:46:46 -0500

    fix: improve loading UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { delay, map, merge, Observable, share, skip, startWith, Subject, switchMap } from 'rxjs';
+import { Component } from '@angular/core';
+import { BehaviorSubject, delay, map, merge, Observable, share, startWith, Subject, switchMap } from 'rxjs';
 import { AffirmationService } from './affirmations/affirmations.service';
 
 @Component({
@@ -7,33 +7,25 @@ import { AffirmationService } from './affirmations/affirmations.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   affirmation$: Observable<string>;
   loading$: Observable<boolean>;
   private affirmationSubject: Subject<void>;
 
   constructor(private affirmationService: AffirmationService) {
-    this.affirmationSubject = new Subject();
+    this.affirmationSubject = new BehaviorSubject<void>(null as any);
     
     this.affirmation$ = this.affirmationSubject
       .pipe(
-        startWith('Loading...'),
         switchMap(() => this.affirmationService.getAffirmation()),
         delay(1000), // Simulate a slow network call
-        share()
+        startWith('Loading...'),
+        share(),
       );
     
     const startLoading$ = this.affirmationSubject.pipe(map(() => true));
-    const stopLoading = this.affirmation$
-      .pipe(
-        skip(1),
-        map(() => false)
-      );
+    const stopLoading = this.affirmation$.pipe(map(() => false));
     this.loading$ = merge(startLoading$, stopLoading);
-  }
-
-  ngOnInit(): void {
-    this.affirmationSubject.next();
   }
 
   onClick(): void {
